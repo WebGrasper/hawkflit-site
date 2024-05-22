6ecbdc12f8ec63355b6a8e10b339bbaaf3d5ef37
WebGrasper <mohdamaan570@gmail.com>
2024-05-22 13:13:45 +0000

    Added google sitemap meta tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="CTCAltmttI3FKYw7b0kceqtkP0QXBnnAUrFBCpTPKa4"
+        />
+      </Head>
       <Navbar />
       <Component {...pageProps} />
       <Footer />
